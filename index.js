753e215b6e2756d0b3451ce5df75182de60b9aa8
illyalane <lane_illya@yahoo.com>
2023-08-30 11:41:07 -0400

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(userName, mail){
+
+    const data = {
+
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: mail,
+        }),
+    }
+    
+    return fetch("http://localhost:3000/users", data)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+    const newId = object.id;
+    const element = document.createElement('p')
+    element.textContent = `New ID: ${newId}`
+    document.body.appendChild(element)
+      })
+      
+      .catch(function (error) {
+        alert("Unauthorized Access");
+        
+        document.body.append(error.message);
+
+      });
+}
+submitData();
